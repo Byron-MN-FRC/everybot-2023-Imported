11bac5699ce49afcb392bbb99948dfc7f785a75c
mhelve46 <miles.helvey@gmail.com>
2026-07-05 18:35:58 -0500

    refactor: remove unused autonomous selection code and related variables

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,19 +15,10 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class Robot extends TimedRobot {
-  /*
-   * Autonomous selection options.
-   */
-  private static final String kNothingAuto = "do nothing";
-  private static final String kConeAuto = "cone";
-  private static final String kCubeAuto = "cube";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   /*
    * Drive motor controller instances.
@@ -59,12 +50,6 @@ public class Robot extends TimedRobot {
 
   /**
    * The starter code uses the most generic joystick class.
-   * 
-   * The reveal video was filmed using a logitech gamepad set to
-   * directinput mode (switch set to D on the bottom). You may want
-   * to use the XBoxController class with the gamepad set to XInput
-   * mode (switch set to X on the bottom) or a different controller
-   * that you feel is more comfortable.
    */
   CommandXboxController m_controller = new CommandXboxController(0);
 
@@ -103,34 +88,10 @@ public class Robot extends TimedRobot {
   static final double INTAKE_HOLD_POWER = 0.07;
 
   /**
-   * Time to extend or retract arm in auto
-   */
-  static final double ARM_EXTEND_TIME_S = 2.0;
-
-  /**
-   * Time to throw game piece in auto
-   */
-  static final double AUTO_THROW_TIME_S = 0.375;
-
-  /**
-   * Time to drive back in auto
-   */
-  static final double AUTO_DRIVE_TIME = 6.0;
-
-  /**
-   * Speed to drive backwards in auto
-   */
-  static final double AUTO_DRIVE_SPEED = -0.25;
-
-  /**
    * This method is run once when the robot is first started up.
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("do nothing", kNothingAuto);
-    m_chooser.addOption("cone and mobility", kConeAuto);
-    m_chooser.addOption("cube and mobility", kCubeAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
 
     /*
      * You will need to change some of these from false to true.
@@ -198,7 +159,6 @@ public class Robot extends TimedRobot {
     arm.set(percent);
     SmartDashboard.putNumber("arm power (%)", percent);
     SmartDashboard.putNumber("arm motor current (amps)", arm.getOutputCurrent());
-    SmartDashboard.putNumber("arm motor temperature (C)", arm.getMotorTemperature());
   }
 
   /**
@@ -215,7 +175,6 @@ public class Robot extends TimedRobot {
 
     SmartDashboard.putNumber("intake power (%)", percent);
     SmartDashboard.putNumber("intake motor current (amps)", intake.getOutputCurrent());
-    SmartDashboard.putNumber("intake motor temperature (C)", intake.getMotorTemperature());
   }
 
   /**
@@ -225,62 +184,6 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     SmartDashboard.putNumber("Time (seconds)", Timer.getFPGATimestamp());
-  }
-
-  double autonomousStartTime;
-  double autonomousIntakePower;
-
-  @Override
-  public void autonomousInit() {
-    frontLeftVictor.setNeutralMode(NeutralMode.Brake);
-    frontRightVictor.setNeutralMode(NeutralMode.Brake);
-    backLeftVictor.setNeutralMode(NeutralMode.Brake);
-    backRightVictor.setNeutralMode(NeutralMode.Brake);
-
-    m_autoSelected = m_chooser.getSelected();
-    System.out.println("Auto selected: " + m_autoSelected);
-
-    if (m_autoSelected == kConeAuto) {
-      autonomousIntakePower = INTAKE_OUTPUT_POWER;
-    } else if (m_autoSelected == kCubeAuto) {
-      autonomousIntakePower = -INTAKE_OUTPUT_POWER;
-    }
-
-    autonomousStartTime = Timer.getFPGATimestamp();
-  }
-
-  @Override
-  public void autonomousPeriodic() {
-    if (m_autoSelected == kNothingAuto) {
-      setArmMotor(0.0);
-      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
-      setDriveMotors(0.0, 0.0);
-      return;
-    }
-
-    double timeElapsed = Timer.getFPGATimestamp() - autonomousStartTime;
-
-    if (timeElapsed < ARM_EXTEND_TIME_S) {
-      setArmMotor(ARM_OUTPUT_POWER);
-      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
-      setDriveMotors(0.0, 0.0);
-    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S) {
-      setArmMotor(0.0);
-      setIntakeMotor(autonomousIntakePower, INTAKE_CURRENT_LIMIT_A);
-      setDriveMotors(0.0, 0.0);
-    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S + ARM_EXTEND_TIME_S) {
-      setArmMotor(-ARM_OUTPUT_POWER);
-      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
-      setDriveMotors(0.0, 0.0);
-    } else if (timeElapsed < ARM_EXTEND_TIME_S + AUTO_THROW_TIME_S + ARM_EXTEND_TIME_S + AUTO_DRIVE_TIME) {
-      setArmMotor(0.0);
-      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
-      setDriveMotors(AUTO_DRIVE_SPEED, 0.0);
-    } else {
-      setArmMotor(0.0);
-      setIntakeMotor(0.0, INTAKE_CURRENT_LIMIT_A);
-      setDriveMotors(0.0, 0.0);
-    }
   }
 
   /**
@@ -304,7 +207,6 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
     double armPower;
-    // TODO: You may want to change this to match  your preferences
     if (m_controller.leftTrigger().getAsBoolean()) {
       // lower the arm
       armPower = -ARM_OUTPUT_POWER;
